deb16b1d89fe249567d8d7b89b5b5e93824fec0a
sando <codemoochim@gmail.com>
2023-03-22 10:52:14 +0900

    Feat: static 미들웨어의 활용

--- ch06/app.js
+++ ch06/app.js
@@ -1,27 +1,25 @@
-// package.json에 type:module 을 추가하여 CJS 를 EMS로 바꾸어보았다
-// const express = require("express");
-// const path = require('path');
 import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
+dotenv.config();
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-console.log(import.meta.url); // file:///Users/sando/Desktop/nodejs101/ch06/app.js
-/**
- * new URL('file:///C:/path/').pathname;      // Incorrect: /C:/path/
- * fileURLToPath('file:///C:/path/');         // Correct:   C:\path\ (Windows)
- * new URL('file://nas/foo.txt').pathname;    // Incorrect: /foo.txt
- * fileURLToPath('file://nas/foo.txt');       // Correct:   \\nas\foo.txt (Windows)
- */
+const index = "/apple/index.html";
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log(dirname); // /Users/sando/Desktop/nodejs101/ch06
 
-// res.send와 res.sendFile 순서 상관 없이 res.send의 우선순위가 더 높다
-app.get("/", (req, res) => {
-  // res.send("Hello, Express");
-  res.sendFile(path.join(dirname, "/index.html"));
+app.use(morgan("dev"));
+
+app.use("/apple", express.static(path.join(dirname, "public-0505")));
+app.get(`${index}`, (req, res) => {
+  // res.send("Hello, Express res.send");
+  res.sendFile(path.join(dirname, "index.html"));
+});
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get("port"), () => {
