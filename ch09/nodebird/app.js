343aa37cb682e1c375327cb0e1c4ff7c07537218
sando <codemoochim@gmail.com>
2023-04-03 13:33:12 +0900

    Feat: 데이터베이스 모델 생성

--- ch09/nodebird/app.js
+++ ch09/nodebird/app.js
@@ -7,4 +7,56 @@ const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const pageRouter = rqeuire("./routes/page");
+const pageRouter = require("./routes/page");
+const { sequelize } = require("./models");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+sequelize
+  .sync({ force: false }) // 서버를 실행할 때마다 테이블을 제생성 할건지?
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", pageRouter);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  err.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_NEW !== "production" ? err : {};
+  res.status(err.stats || 500);
+  res.render("error");
+});
+app.listen(app.get("port"), () => {
+  console.log(`${app.get("port")} 포트 번호로 연결 완료`);
+});
